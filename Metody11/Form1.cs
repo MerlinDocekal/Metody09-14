9a565280a82edb1b8b712ab638557ceba8f6ec4a
docekalm.04 <docekalm.04@A321-16>
2022-03-15 08:55:55 +0100

    Metody11

--- Metody11/Form1.cs
+++ Metody11/Form1.cs
@@ -16,5 +16,30 @@ namespace Metody11
         {
             InitializeComponent();
         }
+
+        private int PocetSlov(string s, out string sBezCislic)
+        {
+            char[] separotor = { ' ' };
+            string[] poleSlov = s.Split(separotor, StringSplitOptions.RemoveEmptyEntries);
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(Char.IsDigit(s[i]))
+                {
+                    s = s.Remove(i, 1);
+                }
+            }
+
+            sBezCislic = s;
+            return poleSlov.Length;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string sBezCislic;
+
+            MessageBox.Show("Počet slov je " + PocetSlov(s,  out sBezCislic) + " a řetězec bez číslic je:\n" + sBezCislic);
+        }
     }
 }
