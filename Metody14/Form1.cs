d41807de22ac2e1d629062382b3d9638c33c4c88
Merlin Dočekal <merlindocekal@gmail.com>
2022-03-19 08:53:37 +0100

    Metody14

--- Metody14/Form1.cs
+++ Metody14/Form1.cs
@@ -16,5 +16,77 @@ namespace Metody14
         {
             InitializeComponent();
         }
+
+        private bool Identicke(string s1, string s2, out int pocetPozic, out int prvniIndex)
+        {
+            bool jsou = true;
+            pocetPozic = 0;
+            bool prvniIndexNalezen = false;
+            prvniIndex = 0;
+            if (s1.Length == s2.Length)
+            {
+                for (int i = 0; i < s1.Length; i++)
+                {
+                    if(s1[i] != s2[i])
+                    {
+                        jsou = false;
+                        pocetPozic++;
+                        if(!prvniIndexNalezen)
+                        {
+                            prvniIndex = i;
+                            prvniIndexNalezen = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                jsou = false;
+
+                int kratsi = s1.Length;
+                if(s2.Length < kratsi)
+                {
+                    kratsi = s2.Length;
+                }
+
+                for (int i = 0; i < kratsi; i++)
+                {
+                    if (s1[i] != s2[i])
+                    {
+                        pocetPozic++;
+                        if (!prvniIndexNalezen)
+                        {
+                            prvniIndex = i;
+                            prvniIndexNalezen = true;
+                        }
+                    }
+                }
+
+                pocetPozic += Math.Abs(s1.Length - s2.Length);
+                if(!prvniIndexNalezen)
+                {
+                    prvniIndex = kratsi;
+                }
+            }
+            return jsou;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            string s2 = textBox2.Text;
+
+            int pocetPozic;
+            int prvniIndex;
+
+            if(Identicke(s1, s2, out pocetPozic, out prvniIndex))
+            {
+                MessageBox.Show("Ano, jsou.");
+            }
+            else
+            {
+                MessageBox.Show("Ne, nejsou. Počet pozic je " + pocetPozic + " a první index odlišnosti je " + prvniIndex);
+            }
+        }
     }
 }
