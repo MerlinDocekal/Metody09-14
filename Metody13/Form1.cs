70c790fbef72f2effce390bf08c4363a5b04b0b3
Merlin Dočekal <merlindocekal@gmail.com>
2022-03-19 08:21:09 +0100

    Metody13

--- Metody13/Form1.cs
+++ Metody13/Form1.cs
@@ -16,5 +16,53 @@ namespace Metody13
         {
             InitializeComponent();
         }
+
+        private bool JeAlfanum(string retezec, out int pocetMalych, out int pocetVelkych, out int pocetJinych)
+        {
+            bool je = true;
+            pocetMalych = 0;
+            pocetVelkych = 0;
+            pocetJinych = 0;
+
+
+            foreach(char c in retezec)
+            {
+                if(Char.IsLower(c))
+                {
+                    pocetMalych++;
+                }
+
+                if (Char.IsUpper(c))
+                {
+                    pocetVelkych++;
+                }
+
+                if (!Char.IsLetterOrDigit(c))
+                {
+                    je = false;
+                    pocetJinych++;
+                }
+            }
+
+            return je;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+
+            int pocetMalych;
+            int pocetVelkych;
+            int pocetJinych;
+
+            if(JeAlfanum(s, out pocetMalych, out pocetVelkych, out pocetJinych))
+            {
+                MessageBox.Show("Ano, je. Počet malých je " + pocetMalych + " a počet velkých je " + pocetVelkych);
+            }
+            else
+            {
+                MessageBox.Show("Ne, není. Počet malých je " + pocetMalych + ", počet velkých je " + pocetVelkych + " a počet jiných je " + pocetJinych);
+            }
+        }
     }
 }
